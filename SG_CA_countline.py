117046be02a5bb5139e829df78a63c276b9873b6
Zasek <zasek415@gmail.com>
2016-03-07 15:29:43 +0800

    OK, cut finished

--- SG_CA_countline.py
+++ SG_CA_countline.py
@@ -4,22 +4,27 @@
 import codecs
 
 INPUT_FILE = "D:\\Grad\\news_xml.dat"
+OUTPUT_FILE = "D:\\Grad\\news_part.dat"
 
 
 def countline():
 
-    input_data = codecs.open(INPUT_FILE, 'r')
-    line = input_data.readline()
-    line = input_data.readline()
-    line = input_data.readline()
-    line = input_data.readline()
-    line = input_data.readline()
+    input_data = codecs.open(INPUT_FILE, 'rb')
+    output_data = codecs.open(OUTPUT_FILE, 'wb')
 
-    line = unicode()
+    count = 0
 
-    print(line)
+    for line in input_data.readlines():
+
+        if (count % 6 == 4) and (count <= 800004):
+            output_data.write(line)
+            count += 1
+        else:
+            count += 1
+            continue
 
     input_data.close()
+    output_data.close()
 
 if __name__ == '__main__':
     countline()
